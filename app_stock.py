b029b17081b5fc9e351b52da199f8f1638dc515f
Sarti <gsarti@deloitte.it>
2022-06-15 13:45:54 +0200

    Upload my second app:app_stock

--- app_stock.py
+++ app_stock.py
@@ -1,25 +1,50 @@
+import pandas as pd
 from flask import Flask
 import dash
-from dash import html
+from dash import html, dcc
+import plotly.express as px
+
+
+
+
+# load data
+df =  pd.read_excel(r'C:\Users\gsarti\OneDrive - Deloitte (O365D)\Desktop\stockdata2.xlsx')
+
 
 app_stock = dash.Dash(__name__)
 
 # Define the app
 app_stock.layout = html.Div()
-# Run the app
-if __name__ == '__main__':
-    app_stock.run_server(debug=True)
+
 
 app_stock.layout = html.Div(children=[
                       html.Div(className='row',  # Define the row element
                                children=[
-                                  html.Div(className='four columns div-user-controls'),  # Define the left element
-                                  html.Div(className='eight columns div-for-charts bg-grey')  # Define the right element
+					 # Define the left elements
+                                  html.Div(className='four columns div-user-controls',
+				  children=[
+					html.H2('Dash - STOCK-PRICES'),
+					html.P('''Visualising time series with Ploty - Dash'''),
+					html.P('''Pick one or more stocks from the dropdown below''')
+					]), 
+					# Define the right element
+                                  html.Div(className='eight columns div-for-charts bg-grey',
+				  children=[
+					dcc.Graph(id="timeseries",
+						 config={'displayModeBar': False},
+          					animate=True,
+          					figure=px.line(df,
+                         				x='Date',
+                         				y='value',
+                         				color='stock',
+                         				template='plotly_dark').update_layout(
+                                   			{'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                    			'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+                                    )])  
                                   ])
                                 ])
 
-children = [
-    html.H2('Dash - STOCK PRICES'),
-    html.P('''Visualising time series with Plotly - Dash'''),
-    html.P('''Pick one or more stocks from the dropdown below.''')
-]
+
+# Run the app
+if __name__== '__main__':
+	app_stock.run_server(debug=True)
