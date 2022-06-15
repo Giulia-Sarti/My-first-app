3291834c2c321dd983e69110e385738686fcc4a8
Sarti <gsarti@deloitte.it>
2022-06-15 14:01:52 +0200

    Continue to work on my app

--- app_stock.py
+++ app_stock.py
@@ -11,6 +11,15 @@ import plotly.express as px
 df =  pd.read_excel(r'C:\Users\gsarti\OneDrive - Deloitte (O365D)\Desktop\stockdata2.xlsx')
 
 
+# define a dictionary for the value
+def get_options(list_stocks):
+    dict_list = []
+    for i in list_stocks:
+        dict_list.append({'label': i, 'value': i})
+
+    return dict_list
+
+
 app_stock = dash.Dash(__name__)
 
 # Define the app
@@ -25,8 +34,19 @@ app_stock.layout = html.Div(children=[
 				  children=[
 					html.H2('Dash - STOCK-PRICES'),
 					html.P('''Visualising time series with Ploty - Dash'''),
-					html.P('''Pick one or more stocks from the dropdown below''')
-					]), 
+					html.P('''Pick one or more stocks from the dropdown below'''),
+					html.Div(
+						className = 'div-for-dropdown',
+						children=[
+							dcc.Dropdown(id="stockselector",options=get_options(df['stock'].unique()),
+									multi = True, value = [df['stock'].sort_values()[0]],
+									style = {'backgroundColor': '#1E1E1E'},
+                                                      			className='stockselector'),
+							],
+							style={'color': '#1E1E1E'}
+						)
+					    ]
+				), 
 					# Define the right element
                                   html.Div(className='eight columns div-for-charts bg-grey',
 				  children=[
